4b4e77922b4f3b0d4d4c72dd20a45d03071c66b4
Excalibur-code <harshkumar9827@gmail.com>
2020-08-23 07:13:21 +0530

    changed

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ io.on("connection", function (socket) {
 })
 // by default-> server's send index.html or main.html first in case of request.
 //expose -> client folder
-app.use(express.static("client"));//used to provide front end whilst making web app...html file will be recieved here.
+app.use(express.static("client"));//used to provide front end(UI) whilst making web app...html file will be recieved here.
 //ln 26-28 will be used if we make a desktop app not a web app.
 // app.get("/", function (req, res) {
 //      res.end("<h1>Welcome to the home page</h1>");///home is the route where information is.
